572641e9e02ccc6f3f4e67026da7cb4a4085858a
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-09-08 11:32:16 +0200

    Make Intro changes
    Signed-off-by: Rafael2026 <rafaaguilarmunoz@gmail.com>

--- Intro/script.js
+++ Intro/script.js
@@ -1,6 +1,5 @@
 // Initialize Image Classifier with MobileNet.
 const classifier =  ml5.imageClassifier('MobileNet');
-
 classifier.classify(document.getElementById("image"), gotResult);
 
 // Function to run when results arrive
