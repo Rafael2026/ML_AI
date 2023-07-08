614c739fb420cdeb641e02963a27c9f3873f5baa
githuddaw1 <rafael.aguilar.alu@iescampanillas.com>
2023-07-08 19:00:19 +0200

    Create NEXTJS app

---
+++ nextjs-dalle-2/src/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import {useState} from 'react';
+
+function Page() {
+
+  //const state
+
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Write your prompt"/>
+        <button>Generate Image</button>
+      </form>
+    </div>
+  )
+}
+
+export default Page
