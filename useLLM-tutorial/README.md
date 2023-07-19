b105ec8c30382a75f380f810ef00c1799e6550d7
githuddaw1 <rafael.aguilar.alu@iescampanillas.com>
2023-07-19 16:01:16 +0200

    USELLM project and more changes

---
+++ useLLM-tutorial/README.md
@@ -0,0 +1,5 @@
+## Fazt Code Web
+Tutorial: [Official link](https://faztweb.com/contenido/usellm-un-hook-de-react-para-crear-chatbots)
+
+## Fazt Code Youtube
+Tutorial: [Official link](https://www.youtube.com/watch?v=1Oi2UwHDG3k)
---
+++ useLLM-tutorial/src/App.jsx
@@ -0,0 +1,49 @@
+import useLLM from "usellm";
+import { useState } from "react";
+
+/*function App() {
+
+  const llm = useLLM({ serviceUrl: "https://usellm.org/api/llm" });
+
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          await llm.chat({
+            messages: [{ role: "user", content: "Who are you?" }],
+            stream: true,
+            onStream: ({ message }) => console.log(message),
+          });
+        }}
+      >
+        Chat
+      </button>
+    </div>
+  );
+}
+
+export default App;*/
+
+export default function Demo() {
+
+  const llm = useLLM({ serviceUrl: "https://usellm.org/api/llm" });
+  const [result, setResult] = useState("");
+ 
+  async function handleClick() {
+    try {
+      await llm.chat({
+        messages: [{ role: "user", content: "What is a language model?" }],
+        stream: true,
+        onStream: ({ message }) => setResult(message.content),
+      });
+    } catch (error) {
+      console.error("Something went wrong!", error);
+    }
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>Send</button>
+      <div style={{ whiteSpace: "pre-wrap" }}>{result}</div>
+    </div>
+  );
+}
